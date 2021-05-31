2869bd05c6bf9174f750d72bf46ef00a4b2373ec
alchoholic-freashman <81649011+alchoholic-freashman@users.noreply.github.com>
2021-05-31 20:06:39 +0900

    Add files via upload

---
+++ repository/BackwardOper/src/kr/dja/BackwardOper/Operator/NumOperand.java
@@ -0,0 +1,15 @@
+package kr.dja.BackwardOper.Operator;
+
+public class NumOperand extends ABS_CalMember
+{// 숫자 피연산자.
+private int value;
+public NumOperand(int v)
+{
+this.value = v;
+}
+public int getValue()
+{
+return this.value;
+}
+@Override public String toString() { return String.valueOf(this.value); }
+}
